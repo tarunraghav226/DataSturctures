e851661c07e80da512b2a896e297b035a12c5dc6
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-14 15:00:57 +0530

    Completed task. Final Commit.

--- arrays/practiceQuestions/src/SieveOfEratosthenesMain.java
+++ arrays/practiceQuestions/src/SieveOfEratosthenesMain.java
@@ -2,5 +2,9 @@ import sieveOfEratosthenes.SieveOfEratosthenes;
 
 public class SieveOfEratosthenesMain {
     public static void main(String[] args) {
+        SieveOfEratosthenes sieve = new SieveOfEratosthenes();
+        sieve.initializeArray();
+        sieve.sieveOfEratosthenes();
+        System.out.println(sieve.toString());
     }
 }
--- arrays/practiceQuestions/src/sieveOfEratosthenes/SieveOfEratosthenes.java
+++ arrays/practiceQuestions/src/sieveOfEratosthenes/SieveOfEratosthenes.java
@@ -1,4 +1,43 @@
 package sieveOfEratosthenes;
 
 public class SieveOfEratosthenes {
+    private boolean[] arr;
+
+    public SieveOfEratosthenes(){
+        arr=new boolean[1000];
+    }
+
+    public void initializeArray(){
+        arr[0]=false;
+        arr[1]=false;
+        for(int i=2;i<arr.length;i++){
+            arr[i]=true;
+        }
+    }
+
+    private boolean isPrime(int num){
+        for(int i=2;i<(int)Math.sqrt(num)+1;i++){
+            if(num%i==0)
+                return false;
+        }
+        return true;
+    }
+
+    public void sieveOfEratosthenes(){
+        for(int i=2;i<arr.length;i++){
+            if(isPrime(i))
+                arr[i]=true;
+            else
+                arr[i]=false;
+        }
+    }
+
+    @Override
+    public String toString(){
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append('|');
+        for(int i=0;i<arr.length;i++)
+            stringBuffer.append(String.valueOf(arr[i])+"|");
+        return stringBuffer.toString();
+    }
 }
