9ef15912fb390022738f6bcfb0ac20da13789b66
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-26 00:58:39 +0530

    Added changes in interface.

--- stack/src/interfaces/StackInterface.java
+++ stack/src/interfaces/StackInterface.java
@@ -2,11 +2,6 @@ package interfaces;
 
 public interface StackInterface<T> {
     /**
-     * Returns the stack.
-     */
-    T[] getStack();
-
-    /**
      * Inserts an object in stack.
      * @param obj The object which is to be inserted.
      */
@@ -17,16 +12,4 @@ public interface StackInterface<T> {
      * @return Returns the removed object from stack.
      */
     T pop();
-
-    /**
-     * Checks whether the stack is having one empty space.
-     * @return Returns the boolean true if stack is having at least one empty space else boolean false.
-     */
-    boolean isEmptySpaceAvailable();
-
-    /**
-     * Checks whether the stack contains at least one data.
-     * @return Returns boolean true if stack is having at least one data in it else boolean false.
-     */
-    boolean isDataAvailable();
 }
