5d820464a4bc0ae8bd85d4f260cbe094d4a387df
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-26 01:51:47 +0530

    Final commit.

--- stack/src/StackMain.java
+++ stack/src/StackMain.java
@@ -1,2 +1,40 @@
+import stack.MyStack;
+
+import java.util.Scanner;
+
 public class StackMain {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter size of Stack");
+        int stackSize = scan.nextInt();
+        MyStack<Integer> stack = new MyStack<>(Integer.class, stackSize);
+
+        while (true) {
+            System.out.println("Do you wants to push y/n");
+            if (scan.next().trim().charAt(0) != 'y')
+                break;
+            System.out.println("Enter Element ");
+            if (stack.push(scan.nextInt()) == -1) {
+                System.out.println("!!!Stack Overflow!!!");
+                break;
+            }
+        }
+
+        for (Integer integer : stack.getStack()) {
+            System.out.print(integer + " ");
+        }
+        System.out.println();
+
+        while (true) {
+            System.out.println("Do you wants to pop y/n");
+            if (scan.next().trim().charAt(0) != 'y')
+                break;
+            Integer integer = stack.pop();
+            if (integer == null) {
+                System.out.println("!!!Stack Is Empty!!!");
+                break;
+            }
+            System.out.println("Popped element--> " + integer);
+        }
+    }
 }
