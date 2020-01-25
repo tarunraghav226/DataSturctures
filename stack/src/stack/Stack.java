b7e34dda57ddfaeed90a9c5a204e033e8d0ae549
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-26 01:05:32 +0530

    Added definition of Stack class.

--- stack/src/stack/Stack.java
+++ stack/src/stack/Stack.java
@@ -1,4 +1,54 @@
 package stack;
 
-public class Stack {
+import interfaces.StackInterface;
+
+public class Stack<T> implements StackInterface<T> {
+    private T[] stack;
+    private int size;
+    private int stackPointer;
+
+    public Stack(){
+        stackPointer=-1;
+    }
+
+    public void setSize(int size){this.size=size;}
+
+    public T[] getStack(){
+        return stack;
+    }
+
+    @Override
+    public void push(T obj){
+        if(isEmptySpaceAvailable()){
+            stackPointer+=1;
+            stack[stackPointer]=obj;
+            return;
+        }
+        System.out.println("!!!Stack Overflow!!!");
+    }
+
+    @Override
+    public T pop(){
+        if(isDataAvailable()){
+            stackPointer--;
+            return stack[stackPointer+1];
+        }
+        return null;
+    }
+
+    /**
+     * Checks whether the stack is having one empty space.
+     * @return Returns the boolean true if stack is having at least one empty space else boolean false.
+     */
+    private boolean isEmptySpaceAvailable(){
+        return stackPointer!=size-1;
+    }
+
+    /**
+     * Checks whether the stack contains at least one data.
+     * @return Returns boolean true if stack is having at least one data in it else boolean false.
+     */
+    boolean isDataAvailable(){
+        return stackPointer>-1;
+    }
 }
