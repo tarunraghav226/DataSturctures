2af57c8802f40e5a3d11f5817afccfc3c4269b12
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-27 10:51:18 +0530

    Completed defintion of LinkedListInterface.

--- linkedList/src/interfaces/LinkedListInterface.java
+++ linkedList/src/interfaces/LinkedListInterface.java
@@ -1,4 +1,38 @@
 package interfaces;
 
 public interface LinkedListInterface<T> {
+
+    /**
+     * Inserts an object in linked list.
+     *
+     * @param obj The object which is to inserted in linked list.
+     */
+    void insertion(T obj);
+
+    /**
+     * Removes the given object from the linked list.
+     *
+     * @param obj The object which is to be removed from the linked list.
+     */
+    void deletion(T obj);
+
+    /**
+     * Searches the object in linked list.
+     *
+     * @param uniqueNumber It is the unique number by which object will be identified.
+     * @return Returns the searched object.
+     */
+    T getObject(int uniqueNumber);
+
+    /**
+     * It updates the object of linked list.
+     *
+     * @param uniqueNumber It is the unique number by which object will be identified.
+     */
+    void update(int uniqueNumber);
+
+    /**
+     * It sorts the linked list.
+     */
+    void sort();
 }
