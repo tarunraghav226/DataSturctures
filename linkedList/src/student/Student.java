56986a9315635898a5048d21260a38bfe3347da4
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-27 11:00:30 +0530

    Completed defintion of Student class.

--- linkedList/src/student/Student.java
+++ linkedList/src/student/Student.java
@@ -1,4 +1,49 @@
 package student;
 
 public class Student {
+    private String name;
+    private int rollNo;
+    private int libraryCode;
+    private String section;
+    private String course;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public int getLibraryCode() {
+        return libraryCode;
+    }
+
+    public void setLibraryCode(int libraryCode) {
+        this.libraryCode = libraryCode;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
 }
