13b613b2c43fc610da57cb2fb02dbdf05c9ff9d6
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-28 17:04:31 +0530

    Completed implementation of insertion method of MyLinkedList class.

--- linkedList/src/myLinkedList/MyLinkedList.java
+++ linkedList/src/myLinkedList/MyLinkedList.java
@@ -1,4 +1,69 @@
 package myLinkedList;
 
-public class MyLinkedList {
+import interfaces.LinkedListInterface;
+import node.MyNode;
+
+public class MyLinkedList implements LinkedListInterface {
+    private MyNode node;
+    private MyNode head = null;
+    private MyNode tail = null;
+
+    public MyNode getNode() {
+        return node;
+    }
+
+    public void setNode(MyNode node) {
+        this.node = node;
+    }
+
+    private MyNode getHead() {
+        return head;
+    }
+
+    private void setHead(MyNode head) {
+        this.head = head;
+    }
+
+    private MyNode getTail() {
+        return tail;
+    }
+
+    private void setTail(MyNode tail) {
+        this.tail = tail;
+    }
+
+    @Override
+    public void insertion(MyNode obj) {
+        if (this.getHead().getNext() == null) {
+            this.setHead(obj);
+            this.setTail(obj);
+        } else {
+            MyNode temp = head;
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(obj);
+            this.setTail(obj);
+        }
+    }
+
+    @Override
+    public void deletion(MyNode obj) {
+
+    }
+
+    @Override
+    public MyNode getObject(int uniqueNumber) {
+        return null;
+    }
+
+    @Override
+    public void update(int uniqueNumber) {
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
 }
