f3c33dc32ff463695b53c5cec685f2cb44a9c8b9
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-29 16:50:13 +0530

    Completed definition of PrimitiveNode class.

--- linkedList/src/node/PrimitiveNode.java
+++ linkedList/src/node/PrimitiveNode.java
@@ -1,4 +1,28 @@
 package node;
 
 public class PrimitiveNode {
+    int data;
+    PrimitiveNode next;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public PrimitiveNode getNext() {
+        return next;
+    }
+
+    public void setNext(PrimitiveNode next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Data --> " + getData() + "\n" +
+                "Next Reference --> " + getNext().hashCode();
+    }
 }
