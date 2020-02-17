3f404e2ac3f3bdec3c402da0c60d1dafdb19e365
tarunraghav226 <tarun.raghav226@gmail.com>
2020-02-17 10:31:19 +0530

    Added getter and setter in LinkedQueue class.

--- queue/src/linkedQueue/LinkedQueue.java
+++ queue/src/linkedQueue/LinkedQueue.java
@@ -1,5 +1,27 @@
 package linkedQueue;
 
+import node.Node;
+
 public class LinkedQueue {
+
+    private Node top;
+    private Node beginning;
+
+    public Node getTop() {
+        return top;
+    }
+
+    public void setTop(Node top) {
+        this.top = top;
+    }
+
+    public Node getBeginning() {
+        return beginning;
+    }
+
+    public void setBeginning(Node beginning) {
+        this.beginning = beginning;
+    }
+
 
 }
