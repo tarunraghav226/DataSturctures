8742ea54578a83a1ec83a13e766bddbb0007d605
tarunraghav226 <tarun.raghav226@gmail.com>
2020-02-19 20:00:50 +0530

    Completed ArrayPriorityQueue class definition.

--- queue/src/priorityQueue/ArrayPriorityQueue.java
+++ queue/src/priorityQueue/ArrayPriorityQueue.java
@@ -8,9 +8,10 @@ public class ArrayPriorityQueue {
     private int rear;
     private PriorityNode[] array;
 
-    public ArrayPriorityQueue() {
+    public ArrayPriorityQueue(int size) {
         front = -1;
         rear = -1;
+        array = new PriorityNode[size];
     }
 
     public int getFront() {
@@ -38,15 +39,51 @@ public class ArrayPriorityQueue {
     }
 
     public void enQueue(PriorityNode newNode) {
+        int i;
         if (front == -1 && rear == -1) {
             front++;
             rear++;
             array[rear] = newNode;
         } else if (rear == array.length - 1) {
             System.out.println("Overflow");
+            return;
         } else {
+            for (i = rear; i >= front; i--) {
+                if (array[i].getPriority() > newNode.getPriority()) {
+                    array[i + 1] = array[i];
+                } else {
+                    break;
+                }
+            }
+            array[i + 1] = newNode;
+            System.out.println("Entered -- " + array[i + 1]);
             rear++;
-            array[rear] = newNode;
         }
+    }
+
+    public PriorityNode deQueue() {
+        PriorityNode temp = new PriorityNode();
+        if (rear == -1 && front == -1) {
+            System.out.println("Underflow");
+            return null;
+        } else if (front == rear) {
+            temp = array[front];
+            front = -1;
+            rear = -1;
+        } else {
+            temp = array[front];
+            front++;
+        }
+        System.out.println("Deleted -- " + temp);
+        return temp;
+    }
+
+    public PriorityNode peek() {
+        return array[front];
+    }
+
+    public void print() {
+        for (int i = 0; i < array.length; i++)
+            System.out.println(array[i]);
     }
 }
