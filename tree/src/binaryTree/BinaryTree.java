322636713a47435b7f9caf312d0da4ad78e856ba
tarunraghav226 <tarun.raghav226@gmail.com>
2020-02-26 16:59:29 +0530

    Added DLR, LDR, LRD method in BinaryTree class.

--- tree/src/binaryTree/BinaryTree.java
+++ tree/src/binaryTree/BinaryTree.java
@@ -1,5 +1,45 @@
 package binaryTree;
 
-public class BinaryTree {
+import node.TreeNode;
 
+public class BinaryTree {
+    private TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode node) {
+        this.root = node;
+    }
+
+    public void DLR(TreeNode node) {
+        if (node == null)
+            return;
+        else {
+            System.out.println(node);
+            DLR(node.getLeftNode());
+            DLR(node.getRightNode());
+        }
+    }
+
+    public void LDR(TreeNode node) {
+        if (node == null)
+            return;
+        else {
+            LDR(node.getLeftNode());
+            System.out.println(node);
+            LDR(node.getRightNode());
+        }
+    }
+
+    public void LRD(TreeNode node) {
+        if (node == null)
+            return;
+        else {
+            LRD(node.getLeftNode());
+            LRD(node.getRightNode());
+            System.out.println(node);
+        }
+    }
 }
