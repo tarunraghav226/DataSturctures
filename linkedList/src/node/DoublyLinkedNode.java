670c1a79b4e7bdc708f56f68581d758c7e53317a
tarunraghav226 <tarun.raghav226@gmail.com>
2020-02-26 17:28:10 +0530

    Completed definition of DoublyLinkedNode class.

---
+++ linkedList/src/node/DoublyLinkedNode.java
@@ -0,0 +1,33 @@
+package node;
+
+import student.Student;
+
+public class DoublyLinkedNode {
+    private Student student;
+    private DoublyLinkedNode previous;
+    private DoublyLinkedNode next;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public DoublyLinkedNode getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(DoublyLinkedNode previous) {
+        this.previous = previous;
+    }
+
+    public DoublyLinkedNode getNext() {
+        return next;
+    }
+
+    public void setNext(DoublyLinkedNode next) {
+        this.next = next;
+    }
+}
