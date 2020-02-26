943ca93945fe81c4df25acead0d7c0e21777620c
tarunraghav226 <tarun.raghav226@gmail.com>
2020-02-26 17:48:31 +0530

    Added DoublyLinkedList class.

--- linkedList/src/myLinkedList/DoublyLinkedList.java
+++ linkedList/src/myLinkedList/DoublyLinkedList.java
@@ -4,6 +4,15 @@ import node.DoublyLinkedNode;
 
 public class DoublyLinkedList {
     private DoublyLinkedNode head;
+    private DoublyLinkedNode tail;
+
+    public DoublyLinkedNode getTail() {
+        return tail;
+    }
+
+    public void setTail(DoublyLinkedNode tail) {
+        this.tail = tail;
+    }
 
     public DoublyLinkedNode getHead() {
         return head;
@@ -16,26 +25,27 @@ public class DoublyLinkedList {
     public void insert(DoublyLinkedNode node) {
         if (getHead() == null) {
             setHead(node);
-            node.setPrevious(getHead());
+            setTail(node);
         } else {
             DoublyLinkedNode temp = getHead();
             while (temp.getNext() != null)
                 temp = temp.getNext();
             temp.setNext(node);
+            setTail(node);
             node.setPrevious(temp);
         }
     }
 
     public void printFromAhead() {
         DoublyLinkedNode temp = getHead();
-        while (temp != null)
+        while (temp != null) {
             System.out.println(temp);
+            temp = temp.getNext();
+        }
     }
 
     public void printFromLast() {
-        DoublyLinkedNode temp = new DoublyLinkedNode();
-        while (temp.getNext() != null)
-            temp = temp.getNext();
+        DoublyLinkedNode temp = getTail();
         while (temp != null) {
             System.out.println(temp);
             temp = temp.getPrevious();
