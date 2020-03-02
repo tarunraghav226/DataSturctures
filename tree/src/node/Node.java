e805a65126f1a7d10603b820e796123c582c724b
tarunraghav226 <tarun.raghav226@gmail.com>
2020-03-02 23:53:57 +0530

    Completed definition of Node class.

--- tree/src/node/Node.java
+++ tree/src/node/Node.java
@@ -1,4 +1,31 @@
 package node;
 
+import student.Student;
+
 public class Node {
+    private Student student;
+    private Node next;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "student=" + student +
+                '}';
+    }
 }
