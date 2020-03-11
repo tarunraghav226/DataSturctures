0945d40af4dec5a73bd208c3ba3ad94cd37c4965
tarunraghav226 <tarun.raghav226@gmail.com>
2020-03-11 20:45:50 +0530

    Added Search method in BinarySearchTree.

--- tree/src/binarySearchTree/BinarySearchTree.java
+++ tree/src/binarySearchTree/BinarySearchTree.java
@@ -1,4 +1,37 @@
 package binarySearchTree;
 
+import node.TreeNode;
+import student.Student;
+
 public class BinarySearchTree {
+    private TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode node) {
+        root = node;
+    }
+
+    public Student search(float cpi) {
+        Student student = new Student();
+        if (root.getStudent().getCpi() == cpi)
+            student = root.getStudent();
+        else {
+            TreeNode temp = root;
+            while (true) {
+                if (temp == null)
+                    break;
+
+                if (Float.compare(cpi, temp.getStudent().getCpi()) == 0)
+                    student = temp.getStudent();
+                if (Float.compare(cpi, temp.getStudent().getCpi()) > 0)
+                    temp = temp.getRightNode();
+                else
+                    temp = temp.getLeftNode();
+            }
+        }
+        return student;
+    }
 }
