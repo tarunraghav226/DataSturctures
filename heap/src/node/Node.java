6d7f98683d7fc407bb6aa362cb2fa730450cdad7
tarunraghav226 <tarun.raghav226@gmail.com>
2020-05-17 19:54:15 +0530

    Updated Node class.

--- heap/src/node/Node.java
+++ heap/src/node/Node.java
@@ -2,7 +2,6 @@ package node;
 
 public class Node {
     private int key;
-    private Node next;
 
     public int getKey() {
         return key;
@@ -16,19 +15,10 @@ public class Node {
         this.key = key;
     }
 
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
     @Override
     public String toString() {
         return "Node{" +
                 "key=" + key +
-                ", next=" + next +
                 '}';
     }
 }
