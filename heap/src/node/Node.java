403efafe63dfcfbe32201e5e83874b01a332b45d
tarunraghav226 <tarun.raghav226@gmail.com>
2020-05-17 19:49:07 +0530

    Added node class.

---
+++ heap/src/node/Node.java
@@ -0,0 +1,30 @@
+package node;
+
+public class Node {
+    private int key;
+    private Node next;
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "key=" + key +
+                ", next=" + next +
+                '}';
+    }
+}
