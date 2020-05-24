67094dc9a7a6c6cd1d98d6d1d6175abd20ee74d9
tarunraghav226 <tarun.raghav226@gmail.com>
2020-05-24 17:56:10 +0530

    Added PriorityQ class.

---
+++ Graph/src/queue/PriorityQ.java
@@ -0,0 +1,58 @@
+package queue;
+
+import edge.Edge;
+
+public class PriorityQ {
+    private final int MAX_SIZE = 20;
+    private Edge[] arr;
+    private int size;
+
+    public PriorityQ() {
+        arr = new Edge[MAX_SIZE];
+        size = 0;
+    }
+
+    public void insert(Edge item) {
+        int j;
+        for (j = 0; j < size; j++)
+            if (item.distance >= arr[j].distance)
+                break;
+        for (int k = j; k < size - 1; k++)
+            arr[k + 1] = arr[k];
+        arr[j] = item;
+        size++;
+    }
+
+    public Edge removeMin() {
+        return arr[--size];
+    }
+
+    public void removeN(int n) {
+        for (int i = n; i < size + 1; i++)
+            arr[i] = arr[i + 1];
+        size--;
+    }
+
+    public Edge peekMin() {
+        return arr[size - 1];
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public Edge peekN(int n) {
+        return arr[n];
+    }
+
+    public int find(int dest) {
+        for (int i = 0; i < size; i++)
+            if (arr[i].destVert == dest)
+                return i;
+        return -1;
+    }
+}
