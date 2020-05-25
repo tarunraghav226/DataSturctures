5a039b3a6d69ddda89150f65f1987cd9e7ea2021
tarunraghav226 <tarun.raghav226@gmail.com>
2020-05-25 12:08:52 +0530

    Updated Edge.

--- Graph/src/edge/Edge.java
+++ Graph/src/edge/Edge.java
@@ -1,9 +1,9 @@
 package edge;
 
 public class Edge {
-    private int srcVert;
-    private int destVert;
-    private int distance;
+    public int srcVert;
+    public int destVert;
+    public int distance;
 
     public Edge(int srcVert, int destVert, int distance) {
         this.srcVert = srcVert;
