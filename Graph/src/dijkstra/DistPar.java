28ce83a2b47d65c35709c4c11ef1044d39db8f5a
tarunraghav226 <tarun.raghav226@gmail.com>
2020-05-30 18:24:10 +0530

    Added DistPar class.

---
+++ Graph/src/dijkstra/DistPar.java
@@ -0,0 +1,11 @@
+package dijkstra;
+
+public class DistPar {
+    int distance;
+    int parent;
+
+    public DistPar(int parent, int distance) {
+        this.distance = distance;
+        this.parent = parent;
+    }
+}
